061c2a58b094d80820a1375e10841a5ca9b2b87b
jc003 <jerryc@berkeley.edu>
2017-04-08 08:59:05 -0700

    updated hw1

--- hw/hw1/hw1.py
+++ hw/hw1/hw1.py
@@ -1,14 +1,14 @@
-###################### POKEMON GO CLOSEST SPAWN POINT ##################################
+###################### POKEMON GO CLOSEST SPAWN POINT ########################################
 #The goal of this assignment is to get you wrangling with numpy and data. In general, 
 #you will be using pandas for 90% of your data analysis needs but for the other 10%,
-#numpy is still used. This task is one of them (okay, you can actually do most of this)
-#in pandas but it's still very good to know how to do with numpy:)).
+#numpy is still used. This first task is one of them (okay, you can actually do most of
+#this in pandas but it's still very good to know how to do with numpy:)).
 
-#Below is a file that contains latitude and longitude coordinates of spawn locations in
-#UC Berkeley's campus. Each spawn locatio spawns a certain pokemon at a particular time
-#each hour according to some distribution. Naturally, standing in a spot with multiple
-#spawn points close to one another would increase the rate of Pokemon to catch. Your
-#second task is to plot these points to get a general sense of how the spawnpoints are
+#Below is a file that contains latitude and longitude coordinates of Pokemon spawn
+#locations in UC Berkeley's campus. Each spawn location spawns a certain pokemon at a
+#particular time each hour according to some distribution. Naturally, standing in a spot
+#with multiple spawn points close to one another would increase the rate of Pokemon to catch.
+#Your second task is to plot these points to get a general sense of how the spawnpoints are
 #scattered.
 
 import numpy as np
@@ -28,7 +28,7 @@ plt.show() #Calls the show function to show the plot in a new window
 
 #FILL ME
 
-#PROBLEM 3 - Find closest two spawn points
+# PROBLEM 3 - Find closest two spawn points
 #It would be great if we can identify a spot in Berkeley that has the densest cluster
 #of spawn points. Identify the two spawn points that are closest to each other out of
 #all spawn points. The distance between two points can be defined in many ways. I 
@@ -54,6 +54,62 @@ for i in range(len(spawns)):
 #the variable spawns.
 # FILL ME
 
+# PROBLEMS 4-6 - Image Convolution
+#Images are often stored as an n x n matrix. This problem will have you read in sample
+#image data on handwritten numbers 0,1,2,3,4,5,6,7,8,9, as well as perform an operation
+#called a "convolution". To read more about how convolution works, visit:
+#http://cs231n.github.io/convolutional-networks/
+#And scroll down to "Convolution Demo" under the Convolution Layer section. They show
+#a visual demonstration of convolving with a 3x3 matrix filter at a stride of 2 (e.g. the
+#filter is moved to the right two steps instead of one). Don't worry about the fancy
+#topics from the other sections. The purpose of this problem is to show you that very
+#complicated techniques can be done with simple numpy array operations (and for loops).
+
+#For those curious, this particular convolution procedure is used to train a machine
+#learning model called a Neural Network. Neural networks are designed to behave much
+#like the human brain and is able "learn" things based on what it sees. The convolution
+#step is one out of many steps to build up this kind of model but it just so happens
+#that this step only requires matrix multiplication and/or array indexing.
+
+# PROBLEM 4 - Read in the image data
+data = np.loadtxt("FILL ME", delimiter=",") #Fill in the file name of your data
+
+# PROBLEM 5 - Visualize the first number
+#Your data array is 10 x (784 + 1). The last column is the label of the number that is
+#represented in each row. For example, row 0 is the digit 5. Notice that each image
+#has been "flattened" down to a vector of 1 x 784. To actually visuzlize this number
+#we need to "reshape" it into a 28x28 array that we are more familiar with. First
+#select the first row but excluding the label column.
+
+img = #FILL ME
+
+#Next reshape the image vector
+
+img_reshaped = np.reshape(img, ) #FILL ME
+
+#Finally we can visualize the image with plt's imshow function.
+
+plt.imshow() #FILL ME
+plt.show()
+
+# PROBLEM 6 - Convolving
+#This problem is open-eneded since there are a couple of ways of doing this. Unlike
+#the demo from the link I gave you above, you are required to convolve with a stride of
+#1 and NO ZERO PADDING is necessary. Your task is to convolve on only the first image
+#(the one your visualized in the last problem). The output should be a 26x26 array.
+
+#Please use this filter kernel to use as you convolve:
+kernel = np.ones((3,3))
+
+#hint: the most intuitive method is to use a couple of for loops (my solution has 4).
+
+#FILL ME
+
+
 #Once you are done with this assignment, please email this file to jerryc@berkeley.edu.
 #Please put [Yeah-HW1] <First Name> <Last Name> as the subject header!
 #Homework 1 is due 4/15 9:30am.
+
+
+
+
